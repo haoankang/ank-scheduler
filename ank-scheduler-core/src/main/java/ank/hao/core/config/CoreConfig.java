bf66f2448254b4996401a6ddeb2fb6ad86bb62b8
haoak <haoak@bsfit.com.cn>
2019-07-23 10:11:24 +0800

    core自动集成配置
    haoak

--- ank-scheduler-core/src/main/java/ank/hao/core/config/CoreConfig.java
+++ ank-scheduler-core/src/main/java/ank/hao/core/config/CoreConfig.java
@@ -1,7 +1,9 @@
 package ank.hao.core.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"ank.hao.core"})
 public class CoreConfig {
 }
